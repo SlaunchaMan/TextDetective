717794d1833931cc4df1b34b9d534ab89ef80f57
Jeff Kelley <SlaunchaMan@gmail.com>
2016-09-18 22:04:57 -0400

    Initial working version.

---
+++ Text Detective/StringInspection.swift
@@ -0,0 +1,87 @@
+//
+//  StringInspection.swift
+//  Text Detective
+//
+//  Created by Jeff Kelley on 9/16/16.
+//  Copyright © 2016 Detroit Labs. All rights reserved.
+//
+
+import CoreFoundation
+import Foundation
+
+enum InspectionResult {
+    case singleCharacter(String)
+    case unicodeDescription(String)
+    
+    var originalString: String {
+        switch self {
+        case let .singleCharacter(string):
+            return string
+        case let .unicodeDescription(string):
+            return String.convert(fromUnicodeDescription: string)
+        }
+    }
+}
+
+extension String {
+    
+    var characterInspection: [InspectionResult] {
+        let cfString = NSMutableString(string: self) as CFMutableString
+        
+        CFStringTransform(cfString,
+                          nil,
+                          kCFStringTransformToUnicodeName,
+                          false)
+        
+        let descriptionString = cfString as String
+        
+        let scanner = Scanner(string: descriptionString)
+        
+        var results: [InspectionResult] = []
+        
+        var buffer: NSString? = NSString()
+        
+        if !descriptionString.hasPrefix("\\N{") {
+            if scanner.scanUpTo("\\N{", into: &buffer) {
+                if let buffer = buffer {
+                    results += (buffer as String)
+                        .characters
+                        .map { return .singleCharacter(String($0)) }
+                }
+            }
+        }
+        
+        while scanner.scanString("\\N{", into: nil) {
+            scanner.scanUpTo("}", into: &buffer)
+            
+            results.append(.unicodeDescription(buffer as? String ?? ""))
+            
+            scanner.scanString("}", into: nil)
+            
+            if scanner.scanUpTo("\\N{", into: &buffer) {
+                if let buffer = buffer {
+                    results += (buffer as String)
+                        .characters
+                        .map { return .singleCharacter(String($0)) }
+                }
+            }
+        }
+    
+        return results
+    }
+    
+    static func convert(fromUnicodeDescription description: String) -> String {
+        let cfString = NSMutableString(string: "\\N{\(description)}")
+            as CFMutableString
+        
+        CFStringTransform(cfString,
+                          nil,
+                          kCFStringTransformToUnicodeName,
+                          true)
+        
+        print("\(cfString as String)")
+
+        return cfString as String
+    }
+    
+}
--- Text Detective/ViewController.swift
+++ Text Detective/ViewController.swift
@@ -9,17 +9,79 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet var tableView: UITableView? {
+        didSet {
+            if let tableView = tableView {
+                tableView.estimatedRowHeight = 44
+                tableView.rowHeight = UITableViewAutomaticDimension
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBOutlet var textField: UITextField?
+    
+    @IBAction func textFieldDidChange(_ sender: UITextField) {
+        characterInspectionResults = sender.text?.characterInspection ?? []
+        tableView?.reloadData()
     }
+    
+    var characterInspectionResults: [InspectionResult] = []
+    
+}
 
+class CodePointCell: UITableViewCell, StoryboardIdentifiable {
+    
+    @IBOutlet var codePointLabel: UILabel?
+    @IBOutlet var descriptionLabel: UILabel?
+    
+    static var storyboardIdentifier: String = "CodePointCell"
+    
+    static func dequeue(fromTableView tableView: UITableView) -> CodePointCell? {
+        return tableView.dequeueReusableCell(withIdentifier: storyboardIdentifier)
+            as? CodePointCell
+    }
+    
+}
 
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return characterInspectionResults.count
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = CodePointCell.dequeue(fromTableView: tableView)
+            else { fatalError() }
+        
+        let unicodeDescription = characterInspectionResults[indexPath.row]
+        
+        cell.codePointLabel?.text = unicodeDescription.originalString
+        
+        switch unicodeDescription {
+        case let .singleCharacter(string):
+            cell.descriptionLabel?.text = string
+        case let .unicodeDescription(string):
+            cell.descriptionLabel?.text = string
+        }
+        
+        return cell
+    }
+    
 }
 
