4f91cd23275b2146230a7527a6d7a047bc8a3ac9
Jeff Kelley <SlaunchaMan@gmail.com>
2016-09-18 22:14:08 -0400

    Support for deleting a row in the table

--- Text Detective/ViewController.swift
+++ Text Detective/ViewController.swift
@@ -28,6 +28,18 @@ class ViewController: UIViewController {
     
     var characterInspectionResults: [InspectionResult] = []
     
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        navigationItem.rightBarButtonItem = editButtonItem
+    }
+    
+    func updateTextFieldText() {
+        textField?.text = characterInspectionResults
+            .map { $0.originalString }
+            .joined(separator: "")
+    }
+    
 }
 
 class CodePointCell: UITableViewCell, StoryboardIdentifiable {
@@ -60,11 +72,6 @@ extension ViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView,
-                   numberOfRowsInSection section: Int) -> Int {
-        return characterInspectionResults.count
-    }
-    
-    func tableView(_ tableView: UITableView,
                    cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = CodePointCell.dequeue(fromTableView: tableView)
             else { fatalError() }
@@ -81,6 +88,24 @@ extension ViewController: UITableViewDataSource {
         }
         
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   commit editingStyle: UITableViewCellEditingStyle,
+                   forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            characterInspectionResults.remove(at: indexPath.row)
+            
+            tableView.deleteRows(at: [indexPath],
+                                 with: .automatic)
+            
+            updateTextFieldText()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return characterInspectionResults.count
     }
     
 }
